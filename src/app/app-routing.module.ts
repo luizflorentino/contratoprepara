223e1bcd882f320b41d99a1c733a399763811931
Luiz Florentino <luizflorentino@gmail.com>
2021-03-12 00:12:26 -0300

    navegacao funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { AlunoCrudComponent } from './views/aluno-crud/aluno-crud.component';
+
+const routes: Routes = [
+  {
+    path: "", component: HomeComponent
+  },
+  {
+    path: "alunos", component: AlunoCrudComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
