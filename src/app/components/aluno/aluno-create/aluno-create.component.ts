49971400e30e1c4fe73334e164b7d831979100d0
Luiz Florentino <luizflorentino@gmail.com>
2021-03-14 16:07:09 -0300

    remocao de codigo desnecessario

--- src/app/components/aluno/aluno-create/aluno-create.component.ts
+++ src/app/components/aluno/aluno-create/aluno-create.component.ts
@@ -4,7 +4,6 @@ import { Aluno } from '../aluno.model';
 import { Escolaridade } from '../../escolaridade/escolaridade.model';
 import { Profissao } from '../../profissao/profissao.model';
 import { AlunoService } from '../aluno.service';
-import { AuxiliaFormsService } from '../../auxilia-forms/auxilia-forms.service';
 import { Endereco } from '../../endereco/endereco.model';
 
 @Component({
@@ -20,41 +19,28 @@ export class AlunoCreateComponent implements OnInit {
 
   constructor(
     private alunoService: AlunoService,
-    private auxiliaFormsService: AuxiliaFormsService,
     private router: Router) { }
 
-  ngOnInit(): void {
-    this.auxiliaFormsService.estadosCivis().subscribe(estadosCivis => {
-      this.estadosCivis = estadosCivis;
-    });
-    this.auxiliaFormsService.sexos().subscribe(sexos => {
-      this.sexos = sexos;
-    });
-  }
+  ngOnInit(): void { }
 
-  getEndereco(endereco: Endereco): void {
+  setEndereco(endereco: Endereco): void {
     this.aluno.endereco = endereco;
-    console.log(this.aluno);
   }
 
-  getEscolaridade(escolaridade: Escolaridade): void {
+  setEscolaridade(escolaridade: Escolaridade): void {
     this.aluno.escolaridade = escolaridade;
-    console.log(this.aluno);
   }
 
-  getProfissao(profissao: Profissao): void {
+  setProfissao(profissao: Profissao): void {
     this.aluno.profissao = profissao;
-    console.log(this.aluno);
   }
 
-  getSexo(sexo: string): void {
+  setSexo(sexo: string): void {
     this.aluno.sexo = sexo;
-    console.log(this.aluno);
   }
 
-  getEstadoCivil(estadoCivil: string): void {
+  setEstadoCivil(estadoCivil: string): void {
     this.aluno.estadoCivil = estadoCivil;
-    console.log(this.aluno);
   }
 
   createAluno(): void {
