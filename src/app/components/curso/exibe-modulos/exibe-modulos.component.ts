850761a55b051afbd856171fd0dbcac08eb9cfc9
Luiz Florentino <luizflorentino@gmail.com>
2021-03-19 19:40:21 -0300

    remocao de modulos para curso personalizado

--- src/app/components/curso/exibe-modulos/exibe-modulos.component.ts
+++ src/app/components/curso/exibe-modulos/exibe-modulos.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Modulo } from '../../modulo/modulo-model';
 import { Curso } from '../curso-model';
 
 @Component({
@@ -13,5 +14,14 @@ export class ExibeModulosComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void { }
+
+  remover(modulo: Modulo): void {
+    const index = this.curso.modulos.indexOf(modulo);
+    console.log(modulo +" - " + index);
+
+    if (index >= 0) {
+      this.curso.modulos.splice(index, 1);
+    }
+  }
 
 }
